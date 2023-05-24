18a66bb0ef40c840e46fe23aa1b9c00ebdd5e809
Aidan Patrick Marias <52266188+Silver0006@users.noreply.github.com>
2023-05-24 16:14:42 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import undetected_chromedriver as uc 
 import chromedriver_autoinstaller
 import time 
+import getpass
 from selenium.webdriver.common.by import By
 version_main = int(chromedriver_autoinstaller.get_chrome_version().split(".")[0])
 
@@ -13,19 +14,24 @@ driver.get("https://member.bookwalker.jp/app/03/login")
 uname = driver.find_element(By.ID, "mailAddress") 
 uname.send_keys(username)  
 
-time.sleep(5) 
+time.sleep(2) 
 passwordF = driver.find_element(By.ID, "password") 
 passwordF.send_keys(password) 
 
+try:
+    driver.find_element(By.NAME, "loginBtn").click() 
+except:
+    driver.find_element(By.ID, "recaptchaLoginBtn").click() 
 
-driver.find_element(By.NAME, "loginBtn").click() 
-
-##recaptchaLoginBtn
 
 time.sleep(2) 
 driver.get("https://global.bookwalker.jp/holdBooks/") 
 time.sleep(2) 
-myName = driver.find_element(By.NAME, "md-book-list md-series-list") 
+myName = driver.find_element(By.XPATH, "//*[@id='pc-hold-books-react-root']")
  
-print("Profile Name: " + myName.get_attribute("innerHTML")) 
+
+sourceFile = open('C:\\Users\\' + getpass.getuser() + '\\Downloads\\New folder\\export\\test.html', 'w', encoding="utf-8")
+print(myName.get_attribute("innerHTML"), file = sourceFile)
+sourceFile.close()
+print(myName.get_attribute, "book-item fn-book-status")
 driver.close()
