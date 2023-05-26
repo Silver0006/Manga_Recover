30cf3a15d95bb0881050c97daad18e721c8251b9
Aidan Patrick Marias <52266188+Silver0006@users.noreply.github.com>
2023-05-25 23:25:14 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
 import undetected_chromedriver as uc 
 import chromedriver_autoinstaller
-import time 
-import getpass
+import time, getpass, os
+from dotenv import load_dotenv
 from selenium.webdriver.common.by import By
-version_main = int(chromedriver_autoinstaller.get_chrome_version().split(".")[0])
 
-username="insert-username"; 
-password="insert-password" 
+load_dotenv()
+username = os.getenv('user_name') 
+password = os.getenv('pass_word')
+
+version_main = int(chromedriver_autoinstaller.get_chrome_version().split(".")[0])
 chromeOptions = uc.ChromeOptions() 
-chromeOptions.headless = True 
-driver = uc.Chrome()
+chromeOptions.add_argument("--headless=new")
+driver = uc.Chrome(uc.ChromeOptions())
 driver.get("https://member.bookwalker.jp/app/03/login") 
 uname = driver.find_element(By.ID, "mailAddress") 
 uname.send_keys(username)  
 
-time.sleep(2) 
+time.sleep(5) 
 passwordF = driver.find_element(By.ID, "password") 
 passwordF.send_keys(password) 
-
+time.sleep(2) 
 try:
     driver.find_element(By.NAME, "loginBtn").click() 
 except:
     driver.find_element(By.ID, "recaptchaLoginBtn").click() 
-
+time.sleep(15) 
+##program workaround for captcha
 
 time.sleep(2) 
 driver.get("https://global.bookwalker.jp/holdBooks/") 
-time.sleep(2) 
-myName = driver.find_element(By.XPATH, "//*[@id='pc-hold-books-react-root']")
- 
+time.sleep(2)  
+myName = driver.find_element(By.CSS_SELECTOR, "div.md-book-list")
 
-sourceFile = open('C:\\Users\\' + getpass.getuser() + '\\Downloads\\New folder\\export\\test.html', 'w', encoding="utf-8")
+sourceFile = open((os.path.dirname(os.path.realpath(__file__))) + '\\export\\test.html', 'w', encoding="utf-8")
 print(myName.get_attribute("innerHTML"), file = sourceFile)
 sourceFile.close()
-print(myName.get_attribute, "book-item fn-book-status")
+## if needed to check html functionality print(myName.get_attribute("innerHTML"))
 driver.close()
